23e3ca8467194019888d25292f01973f0c30c305
aliTanveerAnalog <tanveer@analog.one>
2023-01-20 01:34:26 +0500

    Added support for runtime env config in next

--- next.config.js
+++ next.config.js
@@ -2,4 +2,8 @@
 module.exports = {
   reactStrictMode: true,
   output: 'standalone',
+  publicRuntimeConfig: {
+    NETWORKS: process.env.NEXT_PUBLIC_NETWORKS,
+    INDEXERS: process.env.NEXT_PUBLIC_INDEXERS
+  },
 }
